701e037e3223e583657b2f6fee3426d215a34e7c
G TARUN SAI <119723962+tarunsai31@users.noreply.github.com>
2023-08-10 19:46:25 +0530

    import java.util.*; public class tarun{     public static void main(String args[])     {         Scanner gt=new Scanner(System.in);         int a,b,c;         a=gt.nextInt();         b=gt.nextInt();         c=a*b;         System.out.println(c);     } }

---
+++ Multiplication.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class tarun{
+    public static void main(String args[])
+    {
+        Scanner gt=new Scanner(System.in);
+        int a,b,c;
+        a=gt.nextInt();
+        b=gt.nextInt();
+        c=a*b;
+        System.out.println(c);
+    }
+}
