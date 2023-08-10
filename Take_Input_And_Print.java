cf57ac2b318168798195734274aa82ffdfb176fc
G TARUN SAI <119723962+tarunsai31@users.noreply.github.com>
2023-08-10 20:03:24 +0530

    import java.util.*;  public class tarun { public static void main(String[] args) { Scanner gt= new Scanner(System.in); String str=gt.nextLine(); System.out.println(str); } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,10 @@
+import java.util.*;  
+ public class tarun
+{  
+public static void main(String[] args)  
+{  
+Scanner gt= new Scanner(System.in); 
+String str=gt.nextLine();              
+System.out.println(str);             
+}  
+}  
