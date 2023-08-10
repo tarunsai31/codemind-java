7eafe01a3ce5ec4c28bcf987c7b1fcc5293f9759
G TARUN SAI <119723962+tarunsai31@users.noreply.github.com>
2023-08-10 12:13:10 +0530

    import java.util.*; public class tarun{     public static void main(String args[])     {         Scanner gt=new Scanner(System.in);         int a,b,c;         double s,area;         a=gt.nextInt();         b=gt.nextInt();         c=gt.nextInt();         s=(double)(a+b+c)/2;         area=(Math.sqrt(s*(s-a)*(s-b)*(s-c)));         System.out.printf("%.2f",area);     } }

---
+++ Area_of_Triangle_.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class tarun{
+    public static void main(String args[])
+    {
+        Scanner gt=new Scanner(System.in);
+        int a,b,c;
+        double s,area;
+        a=gt.nextInt();
+        b=gt.nextInt();
+        c=gt.nextInt();
+        s=(double)(a+b+c)/2;
+        area=(Math.sqrt(s*(s-a)*(s-b)*(s-c)));
+        System.out.printf("%.2f",area);
+    }
+}
