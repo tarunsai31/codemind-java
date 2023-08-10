442b102f8710d204018d97a7195f2098625b4684
G TARUN SAI <119723962+tarunsai31@users.noreply.github.com>
2023-08-10 11:59:01 +0530

    import java.util.*; public class tarun{     public static void main(String args[])     {         Scanner gt = new Scanner(System.in);         int r;         double area;         r=gt.nextInt();         area=(double)(3.14*r*r);         System.out.printf("%.2f",area);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class tarun{
+    public static void main(String args[])
+    {
+        Scanner gt = new Scanner(System.in);
+        int r;
+        double area;
+        r=gt.nextInt();
+        area=(double)(3.14*r*r);
+        System.out.printf("%.2f",area);
+    }
+}
