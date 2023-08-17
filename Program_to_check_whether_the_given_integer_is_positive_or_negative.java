aef88f10a9b54e3a1811487e53ce466863a0335d
G TARUN SAI <119723962+tarunsai31@users.noreply.github.com>
2023-08-17 11:25:06 +0530

    import java.util.*; public class tarun{     public static void main(String args[]){         Scanner gt=new Scanner(System.in);         int n;         n=gt.nextInt();         if(n>=0){             System.out.println("Positive Number");         }else{             System.out.println("Negative Number");         }     } }

---
+++ Program_to_check_whether_the_given_integer_is_positive_or_negative.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class tarun{
+    public static void main(String args[]){
+        Scanner gt=new Scanner(System.in);
+        int n;
+        n=gt.nextInt();
+        if(n>=0){
+            System.out.println("Positive Number");
+        }else{
+            System.out.println("Negative Number");
+        }
+    }
+}
