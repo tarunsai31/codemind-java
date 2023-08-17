b95caa44fbbaac63c678f2949d0d37bb600b91ad
G TARUN SAI <119723962+tarunsai31@users.noreply.github.com>
2023-08-17 10:19:49 +0530

    import java.util.*; public class tarun{     public static void main(String args[]){         Scanner gt=new Scanner(System.in);         int n;         n=gt.nextInt();         if(n%2==0){             System.out.println("Even");         }else{             System.out.println("Odd");         }     } }

---
+++ Check_if_number_is_even_or_odd.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class tarun{
+    public static void main(String args[]){
+        Scanner gt=new Scanner(System.in);
+        int n;
+        n=gt.nextInt();
+        if(n%2==0){
+            System.out.println("Even");
+        }else{
+            System.out.println("Odd");
+        }
+    }
+}
