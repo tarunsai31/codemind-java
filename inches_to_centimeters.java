9a81a313fcfb63fafb23fb74ea33ea5441feeffd
G TARUN SAI <119723962+tarunsai31@users.noreply.github.com>
2023-08-25 11:08:25 +0530

    import java.util.*; public class tarun{     public static void main(String args[]){         Scanner gt=new Scanner(System.in);         int in;         in=gt.nextInt();         double cm;         cm=(double)(in*2.54);         System.out.printf("%.2f",cm);
        }
    }

---
+++ inches_to_centimeters.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class tarun{
+    public static void main(String args[]){
+        Scanner gt=new Scanner(System.in);
+        int in;
+        in=gt.nextInt();
+        double cm;
+        cm=(double)(in*2.54);
+        System.out.printf("%.2f",cm);
+        
+    }
+}
