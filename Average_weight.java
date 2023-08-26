48f74be96834f0ef4701124c2c4c3f8f0719bdc3
G TARUN SAI <119723962+tarunsai31@users.noreply.github.com>
2023-08-26 10:27:13 +0530

    import java.util.*; public class  tarun{     public static void main(String args[]){         Scanner gt=new Scanner(System.in);         int x,boy1,boy2,boy3;         x=gt.nextInt();         boy1=gt.nextInt();         boy2=gt.nextInt();         boy3=(3*x)-boy1-boy2;         System.out.println(boy3);     } }

---
+++ Average_weight.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class  tarun{
+    public static void main(String args[]){
+        Scanner gt=new Scanner(System.in);
+        int x,boy1,boy2,boy3;
+        x=gt.nextInt();
+        boy1=gt.nextInt();
+        boy2=gt.nextInt();
+        boy3=(3*x)-boy1-boy2;
+        System.out.println(boy3);
+    }
+}
