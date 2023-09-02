786630f64e87568b19054e71edc6800e99c58a33
G TARUN SAI <119723962+tarunsai31@users.noreply.github.com>
2023-09-02 09:13:58 +0530

    import java.util.*; public class tarun{     public static void main(String args[]){         Scanner gt=new Scanner(System.in);         int s,P,T,R;         P=gt.nextInt();         T=gt.nextInt();         R=gt.nextInt();         s=(P*R*T)/100;         System.out.println(s);
        }
    }

---
+++ SIMPLE_INTEREST.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class tarun{
+    public static void main(String args[]){
+        Scanner gt=new Scanner(System.in);
+        int s,P,T,R;
+        P=gt.nextInt();
+        T=gt.nextInt();
+        R=gt.nextInt();
+        s=(P*R*T)/100;
+        System.out.println(s);
+        
+    }
+}
