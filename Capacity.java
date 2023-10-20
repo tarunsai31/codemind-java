dbea225336c88208a7974e1580764befd6373730
G TARUN SAI <119723962+tarunsai31@users.noreply.github.com>
2023-10-20 09:47:58 +0530

    import java.util.*; public class tarun{     public static void main(String args[]){         Scanner gt=new Scanner (System.in);         int s,t,b;         s=gt.nextInt();         t=gt.nextInt();         b=gt.nextInt();         int c;         c=b*t*s;         System.out.print(c);         System.out.print(" KB");
        }
    }

--- Capacity.java
+++ Capacity.java
@@ -1,12 +1,15 @@
 import java.util.*;
 public class tarun{
     public static void main(String args[]){
-        Scanner gt=new Scanner(System.in);
-        int capacity,b,s,t,KB;
-         b=gt.nextInt();
-         s=gt.nextInt();
-         t=gt.nextInt();
-         capacity=((2*s*t*b*512)/(1024)) ;
-         System.out.println(capacity+" KB");
+        Scanner gt=new Scanner (System.in);
+        int s,t,b;
+        s=gt.nextInt();
+        t=gt.nextInt();
+        b=gt.nextInt();
+        int c;
+        c=b*t*s;
+        System.out.print(c);
+        System.out.print(" KB");
+        
     }
 }
